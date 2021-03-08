dfdaf840e855b824ab44a79d901f356c0e1e3447
dulaya <dulaya.saennok@gmail.com>
2021-03-08 12:05:11 -0800

    added model/user.js

---
+++ model/user.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+var User = mongoose.model('User', userSchema);
+
+export default User;
