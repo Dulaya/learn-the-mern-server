35856b8f6775797a03a42d15b76f866e526f7fe6
dulaya <dulaya.saennok@gmail.com>
2021-03-08 11:35:34 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "5mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello to API');
+})
+
+//Don't forget to use .env
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
