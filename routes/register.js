55e5852c6505813abd4d38f9817dfcb538766866
dulaya <dulaya.saennok@gmail.com>
2021-03-08 12:07:42 -0800

    add example for registering user

--- routes/register.js
+++ routes/register.js
@@ -27,6 +27,40 @@ router.post('/', (request, response) => {
         
     */
 
+    /*
+        Example: 
+
+        //Validate data
+        const { error } = registerValidation(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        //Check if user is alread in database
+        const userExist = await User.findOne({ username: req.body.username });
+        if (userExist) return res.status(400).send('Username already exists');
+
+        //Check if email is alread in database
+        const emailExist = await User.findOne({ email: req.body.email });
+        if (emailExist) return res.status(400).send('Email already exists');
+
+        //Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        //Create a new user
+        const user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashPassword,
+        });
+        try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+        } catch (err) {
+        res.status(400).send(err);
+        }
+
+    */ 
+
 });
 
 export default router;
