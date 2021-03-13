254d6eee3d2f2f73047139861e94a341ffd8a9e0
john Cervantes <johncamilocervantes@gmail.com>
2021-03-13 12:48:05 -0600

    added register API

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import registerRoute from './routes/register.js';
+import registerRoute from "./routes/register.js";
 
 const app = express();
 dotenv.config();
@@ -13,18 +13,21 @@ app.use(bodyParser.json({ limit: "5mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello to API');
+app.get("/", (req, res) => {
+  res.send("Hello to API");
 });
 
-app.use('/register', registerRoute);
+app.use("/register", registerRoute);
 
 //Don't forget to use .env
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
-    .catch((error) => console.log(error.message));
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
--- routes/register.js
+++ routes/register.js
@@ -1,66 +1,58 @@
-import express from 'express';
-
+import express from "express";
 const router = express.Router();
+import User from "../model/user.js";
+import bcrypt from "bcrypt";
 
-router.post('/', (request, response) => {
-    /*
-        handle registration
-        Check if username exists
-        Check if email exists (optional email)
-        Check if password meets requirement
-        Hash password (optional for now, will need later; use bcrypt.genSalt & bcrypt.hash)
-        
-        Create new user, something like: 
-            const user = new User ({
-                username: request.body.username,
-                email: request.body.email,
-                password: hashPassword,
-            })
+router.post("/", async (req, res) => {
+  //Validate data
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message.join("\n"));
 
-        Save to database, something like: 
-            try {
-                const savedUser = await user.save();
-                response.send(savedUser);
-            } catch (error) {
-                response.status(400).send(error);
-            }
-        
-    */
+  //Check if user is alread in database
+  const userExist = await User.findOne({ username: req.body.username });
+  if (userExist) return res.status(400).send("Username already exists");
 
-    /*
-        Example: 
+  //Check if email is alread in database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
 
-        //Validate data
-        const { error } = registerValidation(req.body);
-        if (error) return res.status(400).send(error.details[0].message);
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-        //Check if user is alread in database
-        const userExist = await User.findOne({ username: req.body.username });
-        if (userExist) return res.status(400).send('Username already exists');
-
-        //Check if email is alread in database
-        const emailExist = await User.findOne({ email: req.body.email });
-        if (emailExist) return res.status(400).send('Email already exists');
-
-        //Hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-
-        //Create a new user
-        const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashPassword,
-        });
-        try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-        } catch (err) {
-        res.status(400).send(err);
-        }
-
-    */ 
-
+  //Create a new user
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hashPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
+
+function registerValidation(data) {
+  let error = { details: [{ message: [] }] };
+
+  if (data["username"].trim().length == 0) {
+    error.details[0].message.push("Username is empty");
+  }
+
+  if (data["email"].trim().length == 0) {
+    error.details[0].message.push("Email is empty");
+  }
+
+  if (data["password"].trim().length < 4) {
+    error.details[0].message.push("Password must be atleast 4 characters long");
+  }
+
+  if (error.details[0].message.length != 0) {
+    return { error: error };
+  }
+  return {};
+}
 
 export default router;
